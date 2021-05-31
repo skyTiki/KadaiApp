bf9fa800bae111737fd338094f91b37ed9b5ba51
takatoshi.ichige <takatoshi.ichige>
2021-05-31 13:27:10 +0900

    List画面作成

--- KadaiApp/ViewController.swift
+++ KadaiApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var listTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        listTableView.delegate = self
+        listTableView.dataSource = self
     }
-
-
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath)
+        return cell
+    }
+}
