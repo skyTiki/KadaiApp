6ed3c6ec8b7ce6a32ca31c0e57f960a441e89230
takatoshi.ichige <takatoshi.ichige>
2021-06-01 10:03:01 +0900

    設定画面作成、FireBase追加

---
+++ KadaiApp/ViewControllers/SettingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingViewController.swift
+//  KadaiApp
+//
+//  Created by takatoshi.ichige on 2021/06/01.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tappedCreateDataButton(_ sender: Any) {
+        print("タップデータ作成")
+    }
+}
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '14.2'
+
+target 'KadaiApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for KadaiApp
+  pod 'Firebase','7.5.0'
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Firestore'
+
+end
